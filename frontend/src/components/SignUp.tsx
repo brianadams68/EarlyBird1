4c5b13014bcebc5d5a38a5ad35eb412ae5dc0b09
Brian Adams <brianadamsmaino68@gmail.com>
2024-07-13 15:35:04 +0200

    SignIn and SignUp update

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -24,8 +24,8 @@ const SignUp = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             alt="Your Company"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
+            src="/2.png"
+            className="mx-auto h-20 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Create an Account
